0a2527189cf9b45172c75605bf55344a8a7d5b60
Konrad Renner <koni.renner@gmail.com>
2015-08-06 22:01:29 +0200

    formattierungen

---
+++ Ideen/streifjub/js/main.js
@@ -0,0 +1,22 @@
+
+// ON DOCUMENT READY
+// -----------------------------------------------------------------------------
+$(document).ready(function () {
+
+
+    // FlexSlider
+    // -----------------------------------------------------------------------------
+    $('.main-slider.flexslider, .testimonial-slider.flexslider').flexslider({
+        animation: "slide",
+        direction: "horizontal",
+        slideshow: false,
+        slideshowSpeed: 3500,
+        animationDuration: 500,
+        prevText: "",
+        nextText: "",
+        directionNav: true,
+        controlNav: true
+    });
+
+
+});
