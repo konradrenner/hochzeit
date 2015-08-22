32bda8619f1e9d252e3719c835f0bc61137f73ae
kerstin <kerstin@kerstin-notebook>
2015-08-22 19:05:21 +0200

    navigation, mobiler stempel

--- SinglePageApplication/public_html/js/main.js
+++ SinglePageApplication/public_html/js/main.js
@@ -1,1 +1,33 @@
+function selectHome(){
+    resetNavUnderline();
+    $('#home_item').addClass( 'navtext_selected' );
+}
 
+function selectBrautpaar(){
+    resetNavUnderline();
+    $('#brautpaar_item').addClass( 'navtext_selected' );
+}
+
+function selectAblauf(){
+    resetNavUnderline();
+    $('#ablauf_item').addClass( 'navtext_selected' );
+}
+
+function selectRoute(){
+    resetNavUnderline();
+    $('#route_item').addClass( 'navtext_selected' );
+}
+
+function selectUnterkuenfte(){
+    resetNavUnderline();
+    $('#unterkuenfte_item').addClass( 'navtext_selected' );
+}
+
+function resetNavUnderline(){
+    $('#home_item').removeClass( 'navtext_selected' );
+    $('#brautpaar_item').removeClass( 'navtext_selected' );
+    $('#ablauf_item').removeClass( 'navtext_selected' );
+    $('#route_item').removeClass( 'navtext_selected' );
+    $('#unterkuenfte_item').removeClass( 'navtext_selected' );
+    $('#upload_item').removeClass( 'navtext_selected' );
+}
